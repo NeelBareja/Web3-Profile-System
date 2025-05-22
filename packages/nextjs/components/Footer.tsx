c29fe206ec4be9f351350f73dbeac12ccd02f56c
NeelBareja <75579297+NeelBareja@users.noreply.github.com>
2025-05-22 14:54:23 +0530

    Update Footer.tsx

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import Link from "next/link";
 import { motion } from "framer-motion";
 import { FaGithub, FaTwitter, FaDiscord } from "react-icons/fa";
 
